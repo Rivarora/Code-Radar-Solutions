f625fb3fd14f5d9fdddc539d01a64362bb90bb6f
Rivarora <riva0697.becse24@chitkara.edu.in>
2025-01-30 12:19:39 +0530

    Solution to Check Triangle Validity

---
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int x,y,z,S;
+    scanf("%d %d %d %d",&x,&y,&z,&S);
+    if((S(S-x)(S-y)(S-z))*0.5){
+    printf("Valid");
+}
+else{
+    printf("Invalid");
+}
+return 0;
+}
