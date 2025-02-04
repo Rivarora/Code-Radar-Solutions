c8c74c490213d3948c109fee4edee213d79cb120
Rivarora <riva0697.becse24@chitkara.edu.in>
2025-02-04 15:24:12 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,23 +4,22 @@ int main(){
     int a,,add,sub,mul,div;
     char c;
     scanf("%d%d%c",&a,&b,&c);
-    if(c=='+'){
-        add=a+b;
-        printf("%d",add);
+        if(c=='+'){
+           add=a+b;
+           printf("%d",add);}
         else if(c=='-'){
             sub=a-b;
             printf("%d",sub);
         }
         else if(c=='*'){
             mul=a*b;
-            printf("%d",mul);        }
-    }
-    else if(c=='/'){
-        div=a/b;
-        printf("%d",div);
-    }
-    else{
-        printf("error");
-    }
-    return 0;
+            printf("%d",mul) }
+        else if(c=='/'){
+            div=a/b;
+            printf("%d",div);
+        }
+        else{
+            printf("error");
+        }
+         return 0;
 }
