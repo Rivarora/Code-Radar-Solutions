620ab7a3c719ab7d409a2bb1fecd263b01f1d4c8
Rivarora <riva0697.becse24@chitkara.edu.in>
2025-02-05 21:36:22 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int grade;
+    scanf("%d",&grade);
+    if(grade=="A"){
+        printf("Excellent");
+    }
+    else if(grade=="B"){
+        printf("Good");
+    }
+    else if(grade=="C"){
+        printf("Average");
+    }
+    else if(grade=="D"){
+        printf("Below Averge");
+    }
+    else if(grade=="E"){
+        printf("Fail");
+    }
+    else{
+        printf("Invalid grade");
+    }
+}
