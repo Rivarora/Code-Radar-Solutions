89abcb8e18f7bce3f886814c710dd448df817231
Rivarora <riva0697.becse24@chitkara.edu.in>
2025-02-23 23:24:04 +0530

    Solution to Print a Right-Angled Triangle Pattern

---
+++ Print a Right-Angled Triangle Pattern@@67877e1818d53eb724f435bc/code.c
@@ -0,0 +1,15 @@
+int main(){
+    int row,column
+    scanf("%d %d",&row,&column);
+    for(i=0;i<=row;i++){
+        for(j=0;j<=column;j++){
+            if(j<=i){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+    }
+    return 0;
+}
