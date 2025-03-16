76437b2dbaf92116a5aeb4f775e24d9d3a821945
Rivarora <riva0697.becse24@chitkara.edu.in>
2025-03-16 11:08:11 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,24 +1,17 @@
-void caesarCipher(char message[], int shift, char encrypted[]) {
-    int i = 0;
-    
-    while (message[i] != '\0') {
-        char ch = message[i];
-
-        // Check if the character is a lowercase letter
-        if (ch >= 'a' && ch <= 'z') {
-            encrypted[i] = 'a' + (ch - 'a' + shift) % 26;
+void caserCipher(char message[],int shift,char encrypted[]){
+    int i=0;
+    char ch=message[i];
+    while(meassage[i]!="\0"){
+        if(ch>='a' && ch<='z'){
+            encrypted[i]='a'+((ch-'a')+shift)%26;
         }
-        // Check if the character is an uppercase letter
-        else if (ch >= 'A' && ch <= 'Z') {
-            encrypted[i] = 'A' + (ch - 'A' + shift) % 26;
+        else if(ch>='A' && ch<='Z'){
+            encrypted[i]='A'+((ch-'A')+shift)%26;
         }
-        // Keep other characters unchanged
-        else {
-            encrypted[i] = ch;
+        else{
+            encrypted[i]=ch;
         }
-        
-        i++; // Move to the next character
+        i++;
     }
-
-    encrypted[i] = '\0'; // End the string properly
+    encrypted["\0"];
 }
