c8eecae3ff88d597796a2dbfe97f62021d5e93ef
Rivarora <riva0697.becse24@chitkara.edu.in>
2025-03-29 20:35:34 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

---
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -0,0 +1,10 @@
+int trackPlayerRanks(){
+    int ranked,player,result;
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            if(player[i]>player[i+1]){
+                result=player[i];
+            }
+        }
+}
+}
