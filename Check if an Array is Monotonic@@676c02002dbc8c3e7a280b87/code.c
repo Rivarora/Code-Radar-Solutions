21d5d68d42d6d17381e3cb28acf7b73f5979512b
Rivarora <riva0697.becse24@chitkara.edu.in>
2025-03-30 15:42:37 +0530

    Solution to Check if an Array is Monotonic

--- Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
+++ Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
-int main(){
-    int N,i,max,min,flag=0;
-    scanf("%d",&N);
-    int arr[N];
-    for(i=0;i<N;i++){
-    scanf("%d",&arr[i]);}
-    max=arr[0],min=arr[0];
+
+int main() {
+    int N, i, max, min;
+    scanf("%d", &N);
     
-    for(i=0;i<N;i++){
-        if(N==1){
-            printf("YES");
-            return 0;
-        }
-        if(arr[i]>=max){
-            max=arr[i];
-            flag=1;
-        }
-        if(arr[i]<=min){
-            min=arr[i];
-            flag=1;
-        }
+    int arr[N];
+    for (i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
     }
-    if(flag==1){
+
+    if (N == 1) {  
         printf("YES");
+        return 0;
     }
-    else{
-        printf("NO");
+
+    max = arr[0];
+    min = arr[0];
+
+    for (i = 1; i < N; i++) {  
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
     }
+
+    printf("Max: %d, Min: %d", max, min);
+
+    return 0;
 }
