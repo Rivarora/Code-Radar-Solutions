51e2c30017d8d23a5ec9b966ce36d28be4f6dd11
Rivarora <riva0697.becse24@chitkara.edu.in>
2025-03-30 17:44:40 +0530

    Solution to String Compression

---
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -0,0 +1,10 @@
+int compressString(int str[],int compressed){
+     int len;
+     len=strlen(str);
+     if(str[i]==str[i+1]){
+        printf("%c %d",str[0],len);
+     }
+     else{
+        printf("%s",str);
+     }
+}
